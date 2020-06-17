38dcedcf825be3547b797dc2aab60312a1ec08c0
m-dantas <mauriciodantas.contato01@bol.com.br>
2020-06-16 21:45:26 -0300

    feat: add seeder usuarios para teste

---
+++ seeders/001-usuarios.js
@@ -0,0 +1,26 @@
+const now = new Date()
+const { hashSync } = require('bcrypt')
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'usuarios',
+      [
+        {
+          nome: 'Maurício',
+          sobrenome: 'Correia Dantas',
+          email: 'mauricio.dantascp@gmail.com',
+          password:'$2b$10$4P9ofTvzLRHdVAtWJr4dAunskgYIgT1QAXePqIWKvL0P7IzhdZX0u', // 123456
+          nomeEmpresa: 'Endeavor Comp.',
+          cnpj: '12345678',
+          createdAt: now,
+          updatedAt: now
+        },
+      ],
+      {}
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('usuarios', null, {})
+  }
+}
