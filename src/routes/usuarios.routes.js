b91f8ed23d3ae651eb59e0ecca74fc0b3ee743d2
m-dantas <mauriciodantas.contato01@bol.com.br>
2020-06-19 21:54:54 -0300

    feat: add endpoints login and logout

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,11 +1,14 @@
+const router = require('express').Router()
+const usuarios = require('../controllers/usuarios.controller.js')
+
 module.exports = (usuario, base) => {
-  const usuarios = require('../controllers/usuarios.controller.js')
-  const router = require('express').Router()
-  router.post('/', usuarios.create)
+  router.post('/', usuarios.create) // registro
+  router.post('/login', usuarios.login)
+  router.delete('/logout', usuarios.logout)
+
   router.get('/', usuarios.findAll)
   router.get('/:id', usuarios.findOne)
   router.put('/:id', usuarios.update)
   router.delete('/:id', usuarios.delete)
   usuario.use(`${base}/usuarios`, router)
 }
-
